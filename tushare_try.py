2cbba4312ec777a6f8cd794888e9bd963817eaa5
Zhou-Yankai <zhouyankai2000@yahoo.com>
2024-01-31 22:12:52 +0000

    first overview

--- tushare_try.py
+++ tushare_try.py
@@ -1,14 +1,19 @@
-import tushare as ts
-import pandas as pd
+# import tushare as ts
+# import pandas as pd
 
 
-# 设置tushare pro的token并初始化接口
-pro = ts.pro_api()
-ts_token = ts.set_token('0a14e93298e2c6f043629fe5488be9aebbaf106ad85a3d2d19d19a82')
-pro = ts.pro_api(ts_token)
+# # 设置tushare pro的token并初始化接口
+# pro = ts.pro_api()
+# ts_token = ts.set_token('0a14e93298e2c6f043629fe5488be9aebbaf106ad85a3d2d19d19a82')
+# pro = ts.pro_api(ts_token)
 
-# 获取数据
-df = pro.trade_cal(exchange='', start_date='20180901', end_date='20181001', fields='exchange,cal_date,is_open,pretrade_date', is_open='0')
+# # 获取数据
+# df = pro.trade_cal(exchange='', start_date='20180901', end_date='20181001', fields='exchange,cal_date,is_open,pretrade_date', is_open='0')
 
-# 查看数据
-print(df.head())
+# # 查看数据
+# print(df.head())
+
+x = '2021'
+y = '2020'
+z = str(int(x) + int (y))
+print(z)
--- yfinance_try.py
+++ yfinance_try.py
@@ -19,44 +19,60 @@ aapl = yf.Ticker("AAPL")
 
 # 获取历史市场数据
 comp_overview = aapl.info['longBusinessSummary']
-print(comp_overview)
+# print(comp_overview)
 
 income_statement_annual = aapl.financials.T
 revenue = income_statement_annual['Total Revenue']
 net_income = income_statement_annual['Net Income']
 # revenue_growth_rate = revenue.pct_change() * 100
 # net_income_growth_rate = net_income.pct_change() * 100
+income_statement = aapl.financials
 
-year = '2022'
-year_1 = '2021'
-revenue_year = float(revenue[year])
-net_income_year = float(net_income[year])
-revenue_growth_rate_year = float(revenue[year])/float(revenue[year_1]) * 100 - 100
-net_income_growth_rate_year = float(net_income[year])/float(net_income[year_1]) * 100 - 100
-print("Income Statement (Annual):")
-# print(income_statement_annual)
-print(f"营收（{year}）: {revenue_year}")
-print(f"净利润（{year}）: {net_income_year}")
-print(f"营收增长率（{year}）: {revenue_growth_rate_year}%")
-print(f"净利润增长率（{year}）: {net_income_growth_rate_year}%")
+# print(income_statement)
 
-rate = revenue.pct_change() * 100
-print(f"同比增速为{rate}")
+year = '2023'
+year_1 = str(int(year)-1)
+year_2 = str(int(year)-2)
+his_year = [year,year_1,year_2]
+print(his_year)
+financial_summary_3years = []
 
-financial_summary = f"""
-苹果公司的2022年财务数据如下：
-- 营收: {revenue_year}
-- 净利润: {net_income_year}
-- 营收增长率: {revenue_growth_rate_year}%
-- 净利润增长率: {net_income_growth_rate_year}%
-"""
+for y in his_year:
+    revenue_year = float(revenue[y])
+    net_income_year = float(net_income[y])
+    y_1 = str(int(y)-1)
+    revenue_year_1 = revenue[y_1]
+    net_income_year_1 = net_income[y_1]
+    revenue_year_1 = float(revenue_year_1)
+    net_income_year_1 = float(net_income_year_1)
+    revenue_growth_rate_year = revenue_year/revenue_year_1 * 100 - 100
+    net_income_growth_rate_year = net_income_year/net_income_year_1 * 100 - 100
+    # print("Income Statement (Annual):")
+    # print(income_statement_annual)
+    # print(f"营收（{y}）: {revenue_year}")
+    # print(f"净利润（{y}）: {net_income_year}")
+    # print(f"营收增长率（{y}）: {revenue_growth_rate_year}%")
+    # print(f"净利润增长率（{y}）: {net_income_growth_rate_year}%")
 
-# prompt = comp_overview + "\n" + financial_summary + "\n基于以上信息，请作为一个股票分析师的角色从专业的金融视角分析与总结公司概况其中包括公司的简介与近来的财务概况,财务数据的单位是美元"
 
-# response = client.completions.create(
-#   model="gpt-3.5-turbo-instruct",
-#   prompt=prompt,
-#   max_tokens=600
-# )
+    financial_summary = f"""
+    苹果公司的{y}年财务数据如下：
+    - 营收: {revenue_year}
+    - 净利润: {net_income_year}
+    - 营收增长率: {revenue_growth_rate_year}%
+    - 净利润增长率: {net_income_growth_rate_year}%
+    """
+    # print(financial_summary)
 
-# print(response)
+    financial_summary_3years.append(financial_summary)
+print(financial_summary_3years)
+
+prompt = comp_overview + "\n" + str(financial_summary_3years) + "\n基于以上信息，请作为一个股票分析师的角色从专业的金融视角分析与总结公司概况其中包括公司的简介与近来的财务概况与变化原因,财务数据的单位是美元"
+
+response = client.completions.create(
+  model="gpt-3.5-turbo-instruct",
+  prompt=prompt,
+  max_tokens=1000
+)
+
+print(response)
